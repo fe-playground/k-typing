235811eaf752ddf031eb0e870657c78bac7fc669
1004w455 <1004w455@naver.com>
2018-09-17 22:16:08 +0900

    타자연습기 1차

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Typing
+      v-for="(w, i) in words"
+      :text="w"
+      :index="i"
+      :onSuccess="onSuccess"
+      :active="currentIndex === i"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Typing from './components/Typing.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Typing
+  },
+  data: () => {
+    return {
+      words: [
+        '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세',
+        '무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세'
+      ],
+      currentIndex: 0,
+    }
+  },
+  methods: {
+    onSuccess (index) {
+      this.currentIndex = index + 1
+    }
   }
 }
 </script>
---
+++ src/components/Typing.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="typing">
+    <div class="view-line">{{ text }}</div>
+    <input
+      class="editor"
+      type="text"
+      v-model="editor"
+      @keydown.enter="handleEnter"
+      :disabled="!active"
+      ref="editorInput"
+      :autofocus="active"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Typing',
+  props: {
+    text: String,
+    index: Number,
+    active: Boolean,
+    onSuccess: Function,
+  },
+  data: function () {
+    return {
+      editor: ''
+    }
+  },
+  methods: {
+    handleEnter (e) {
+      if (this.editor === this.text) {
+        this.onSuccess(this.index)
+      }
+    }
+  },
+  watch: {
+    active () {
+      if (this.active) {
+        console.log(123123, this.$refs.editorInput)
+        this.$refs.editorInput.focus()
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.typing {
+  width: 600px;
+  margin: 0 auto;
+  margin-bottom: 10px;
+  text-align: left;
+  .view-line {
+
+  }
+  .editor {
+    width: 100%;
+  }
+}
+</style>
